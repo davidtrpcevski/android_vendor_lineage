57a537d5733e8d89a340d610bcb7dda3768a04da
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-01 19:54:23 -0300

    vendor: Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS
    Change-Id: I18db7cc09ca93bcbc268e5de7230117ead321634
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -244,7 +244,8 @@ endif
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
-    TrebuchetQuickStep
+    TrebuchetQuickStep \
+    NexusLauncherRelease
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
